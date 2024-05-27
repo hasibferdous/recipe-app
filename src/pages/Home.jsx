222a6c4fc8d3dbd9ab4a33c12571203de9670a72
hasibferdous <hasibferdous.ict@gmail.com>
2024-05-27 16:35:18 +0600

    data load from json sever

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,60 @@
+import { useEffect } from "react";
 import Banner from "../components/home/Banner";
+import { useState } from "react";
+import RecipeCard from "../components/cards/RecipeCard";
+import CategoryCard from "../components/cards/CategoryCard";
 
-const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
+export default function Home() {
+  const [recipes, setRecipes] = useState();
+  const [categories, setCategories] = useState();
+  useEffect(() => {
+    // fetch("http://localhost:3000/recipes")
+    //   .then((res) => res.json())
+    //   .then((data) => setRescipes(data));
+
+    async function load() {
+      //get recipies
+      const recipeRes = await fetch("http://localhost:3000/recipes");
+      const recipeData = await recipeRes.json();
+      setRecipes(recipeData);
+      //get categories
+
+      const categoryRes = await fetch("http://localhost:3000/categories");
+      const categoryData = await categoryRes.json();
+
+      setCategories(categoryData);
+    }
+    load();
+
+    // fetch("http://localhost:3000/categories")
+    //   .then((res) => res.json())
+    //   .then((data) => setCategories(data));
+  }, []);
+
+  console.log("hi");
+  return (
+    <div>
+      <Banner />
+
+      <div className="mx-16">
+        <h1 className="text-4xl my-20 text-center">Our Recipe Categories </h1>
+        <div className="grid grid-cols-4 gap-6">
+          {categories?.map((category) => (
+            <CategoryCard key={category?.id} category={category} />
+          ))}
         </div>
-    );
-};
-
-export default Home;
+      </div>
+      <div className="mx-16">
+        <h1 className="text-4xl my-20 text-center">Our Newest Recipes </h1>
+        <div className="grid grid-cols-4 gap-6">
+          {recipes
+            ?.reverse()
+            ?.slice(0, 4)
+            ?.map((recipe) => (
+              <RecipeCard key={recipe?.id} recipe={recipe} />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
